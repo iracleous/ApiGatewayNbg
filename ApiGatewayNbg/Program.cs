c1ff0ceb15c4feae3c5a4b46afb27fa2f7264eb5
dimitris <iracleous@gmail.com>
2024-04-01 09:49:52 +0300

    Congiguring ocelot

--- ApiGatewayNbg/Program.cs
+++ ApiGatewayNbg/Program.cs
@@ -1,8 +1,20 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//1
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+                            .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                            ;
+//2
+builder.Services.AddOcelot();
+
+
 
 var app = builder.Build();
 
@@ -13,5 +25,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+//3
+app.UseOcelot().Wait();
 app.Run();
