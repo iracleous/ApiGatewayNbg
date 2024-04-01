38dd7de3a83e15470123c34c5475cdbf23f1a9b3
dimitris <iracleous@gmail.com>
2024-04-01 09:46:28 +0300

    Add project files.

---
+++ ApiProduct/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using ApiProduct.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiProduct.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return [ 
+                new Product { Id = 1, Name = "chips" } ,
+           new Product { Id = 2, Name = "choco" } ];
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public Product Post([FromBody] Product value)
+        {
+            return value;
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
